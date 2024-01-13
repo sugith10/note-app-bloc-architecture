745bf8ae6163d61c00d9c5f2ae47fd455c16def5
Sugith <sugithkodappurath@gmail.com>
2024-01-13 10:05:21 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:new_note/view/screen_all_notes.dart';
+import 'package:new_note/screen/home_screen.dart';
 
 main(){
   runApp(MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AllNotesScrn(),
+      home: HomeScreen(),
     );
   }
 }
